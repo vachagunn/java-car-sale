e800000e27cd8cc247f9245f2970d30b34e7b59a
Vachik <55889660+vachagunn@users.noreply.github.com>
2024-01-22 17:54:40 +0500

    Попытка добавить изображения (не работает).

--- src/main/java/com/example/CarSale/entities/Product.java
+++ src/main/java/com/example/CarSale/entities/Product.java
@@ -58,4 +58,9 @@ public class Product {
     private void init() {
         dateOfCreated = LocalDateTime.now();
     }
+
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
 }
--- src/main/java/com/example/CarSale/services/ProductService.java
+++ src/main/java/com/example/CarSale/services/ProductService.java
@@ -1,11 +1,14 @@
 package com.example.CarSale.services;
 
+import com.example.CarSale.entities.Image;
 import com.example.CarSale.entities.Product;
 import com.example.CarSale.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,14 +17,48 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+
     public List<Product> listProducts(String title) {
         if (title != null) return productRepository.findByTitle(title);
         return productRepository.findAll();
     }
 
-    public void saveProduct(Product product) {
-        log.info("Saving new {}", product);
+    public void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+        Image image1;
+        Image image2;
+        Image image3;
+
+        if (file1.getSize() != 0) {
+            image1 = toImageEntity(file1);
+            image1.setPreviewImage(true);
+            product.addImageToProduct(image1);
+        }
+
+        if (file2.getSize() != 0) {
+            image2 = toImageEntity(file2);
+            product.addImageToProduct(image2);
+        }
+
+        if (file3.getSize() != 0) {
+            image3 = toImageEntity(file3);
+            product.addImageToProduct(image3);
+        }
+
+        log.info("Saving new Product. Title: {}; Author: {}", product.getTitle(), product.getAuthor());
+        Product productFromDb = productRepository.save(product);
+        productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
         productRepository.save(product);
+    }
+
+    private Image toImageEntity(MultipartFile file1) throws IOException {
+        Image image = new Image();
+        image.setName(file1.getName());
+        image.setOriginalFileName(file1.getOriginalFilename());
+        image.setContentType(file1.getContentType());
+        image.setSize(file1.getSize());
+        image.setBytes(file1.getBytes());
+
+        return image;
     }
 
     public void deleteProduct(Long id) {
